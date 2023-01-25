56182638774aab12868fd6f84ccd30367674d616
VidyanandSawai17 <vidyanandsawai17@gmail.com>
2023-01-25 10:09:44 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment13
+{
+    public class Employee // base class / parent class
+    {
+        protected int id;
+        protected string name;
+        protected double basic, bonus, gross;
+        private static int count;
+        public Employee(string name, double basic)
+        {
+            count++;
+            id = count;
+            this.name = name;
+            this.basic = basic;
+        }
+
+        public virtual void CalculateSalary()
+        {
+            bonus = basic * 0.5;
+            gross = basic + bonus;
+        }
+        public override string ToString()
+        {
+            return $"\n Id ={id} \n Name ={name} \n Basic Salary ={basic} \n Gross of Employee ={gross}";
+        }
+    }
+
+    public class SalesPerson : Employee   // imheritance --> child class and derived class 
+    {
+        private double comm, bonus ;
+
+
+        // base keyword will call the base class constructor
+
+
+        public SalesPerson(string name, double basic) : base(name, basic)
+        {
+
+        }
+        public override void CalculateSalary()
+        {
+           
+            comm = basic * 0.05;
+            bonus = basic * 0.5;
+
+           
+            gross = basic + comm + bonus;
+
+        }
+        public override string ToString()
+        {
+            return $"\n Id ={id} \n Name ={name} \n Basic Salary ={basic} \n SalesPerson ={gross}";
+        }
+    }
+
+}
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment13
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            //  Polymorphism: → it is a mechanism in which multiple objects have a same behavior but having different implementation is called polymorphism
+
+
+            Employee e1 = new Employee("Vivekanand", 17000); //object 1
+            e1.CalculateSalary();
+            Console.WriteLine(e1);
+
+            SalesPerson m1 = new SalesPerson("Vidyanand", 41000); // object 2
+            m1.CalculateSalary();
+            Console.WriteLine(m1);
+        }
+
+    }
+}
